5e8b6a7b9db5dda88dc225cde5a5712fb00a0b32
Dhanalakshmi Jothi <91247537+dhana2403@users.noreply.github.com>
2025-06-10 22:07:14 +0200

    Add files via upload

---
+++ utils/dataset_utils.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+
+class HAM10000Dataset(Dataset):
+    def __init__(self, dataframe, image_dir, transform=None):
+        self.metadata = dataframe
+        self.image_dir = image_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+        image_path = os.path.join(self.image_dir, row['image_id'] + '.jpg')
+        image = Image.open(image_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = row['binary_label']
+        return image, label
+
+def prepare_binary_labels(csv_path):
+    df = pd.read_csv(csv_path)
+    benign_labels = ['nv', 'bkl', 'df', 'vasc']
+    malignant_labels = ['akiec', 'bcc', 'mel']
+    label_map = {label: 0 for label in benign_labels}
+    label_map.update({label: 1 for label in malignant_labels})
+    df['binary_label'] = df['dx'].map(label_map)
+    return df
+
+def handle_missing_values(csv_path):
+    df = pd.read_csv(csv_path)
+    df['age'] = df['age'].fillna(df['age'].median())
+    return df
